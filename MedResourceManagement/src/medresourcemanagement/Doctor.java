0b1e63162724d034334effb7a3ed0d971d47b3b6
Gordon Hall <Hall-G9@email.ulster.ac.uk>
2017-11-17 11:38:28 +0000

    here we go

---
+++ MedResourceManagement/src/medresourcemanagement/Doctor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medresourcemanagement;
+
+/**
+ *
+ * @author b00720507
+ */
+public abstract class Doctor {
+    public String firstName;
+    public String lastName;
+    //language?
+    
+   
+}
